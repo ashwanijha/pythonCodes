e1af505a5de8de109643cb271a6117a71754299b
Ashwani Jha <ashwanijha.bioinfo@gmail.com>
2023-04-21 09:29:40 +0530

    Format coloumn

--- matchAnnotations.py
+++ matchAnnotations.py
@@ -1,19 +1,17 @@
 # %%
 import pandas as pd
-inputFolder = "D:/Covid_data_New/Yr2023/March/20March/Res/"
-annotationFile = pd.read_excel(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230320_1227PM.xlsx")
+inputFolder = "D:/Covid_data_New/Yr2023/4April/14April/PKK/"
+annotationFile = pd.read_excel(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230414_1136AM.xlsx")
 
 
 # %%
 for i in annotationFile.index:
-    splitPattern = "_20230126"
-    if "_20230320" in annotationFile.loc[i,"ExcelFileName"]: 
-        splitPattern = "_20230320"
-    string = annotationFile.loc[i,"ExcelFileName"].split(splitPattern)
+    #splitPattern = "_20230329"
+    splitPattern = ".eds"
+    string = annotationFile.loc[i,"FileName"].split(splitPattern)
     wellAnnotation = annotationFile.loc[i,"Well_Position"]
     targetAnnotation = annotationFile.loc[i,"Target"]
     csvFile = string[0]+".csv"
-    #print(csvFile)
     try:
         csvFileReader = pd.read_csv(inputFolder+"/"+csvFile)
         match = 0
@@ -29,15 +27,14 @@ for i in annotationFile.index:
     except:
         annotationFile.at[i,"Clean|ProblemSevere|ProblemLite"] = "Clean"
 
-    #
     if annotationFile.loc[i,"AmpType (Clear|Strong|Weak|NoAmp)"] == "noAmp":
         annotationFile.at[i,"AmpStatus (Amp|Non_Amp|Unknown) -Annotator"] = "Non_Amp"
     else:
         annotationFile.at[i,"AmpStatus (Amp|Non_Amp|Unknown) -Annotator"] = "Amp"
     
-    annotationFile.at[i,"Annotator"] = "Puneet"
+    annotationFile.at[i,"Annotator"] = "Ashwani"
         
-writer = pd.ExcelWriter(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230307_PK.xlsx")
+writer = pd.ExcelWriter(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230414_AJ.xlsx")
 annotationFile.to_excel(writer,sheet_name="data",index=False)
 writer.close()
 

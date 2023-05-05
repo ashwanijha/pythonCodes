dee537ccf6bdd1e220e441632a94f4a4e908becd
Ashwani Jha <ashwanijha.bioinfo@gmail.com>
2023-05-05 13:31:02 +0530

    updated annotation code

--- matchAnnotations.py
+++ matchAnnotations.py
@@ -1,7 +1,57 @@
 # %%
 import pandas as pd
-inputFolder = "D:/Covid_data_New/Yr2023/4April/14April/PKK/"
-annotationFile = pd.read_excel(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230414_1136AM.xlsx")
+
+inputFolder = "D:/Covid_data_New/Yr2023/5May/5May/Res/" # Outfolder of Hari's Script
+inputFolderExcels = "D:/Covid_data_New/Yr2023/5May/5May/Excels"  # DA exported excels
+
+def wellResultFile(inputFiles,inputFolderExcels):
+    excelFiles = pd.read_excel(inputFiles, sheet_name="Amplification Data",header=None)
+    edsFile = excelFiles[:1]
+    edsFileName = edsFile[1][0].split("\\")
+    csvFile = edsFileName[-1].split(".eds")
+    ofile = open(inputFolderExcels+"/"+csvFile[0]+".csv","w")
+    ofile.write("Well,Target\n")
+    completeData = excelFiles[23:]
+    
+    completeData1 = pd.DataFrame(completeData.values[1:],columns= completeData.iloc[0])
+    wellPositon = {}
+    for i in completeData1.index:
+        cycleNumber = completeData1.loc[i,"Cycle Number"]
+        dRN = float(completeData1.loc[i,"dRn"])
+        if cycleNumber == 1:
+            if dRN < -20000:
+                #print(excelFiles.loc[i,"Well Position"],excelFiles.loc[i,"Target"],excelFiles.loc[i,"dRn"])
+                wellTarget = completeData1.loc[i,"Well Position"]+"\t"+completeData1.loc[i,"Target"]
+                wellPositon[wellTarget] = dRN
+
+    for i in wellPositon:
+        a = i.split("\t")
+        ofile.write(a[0]+","+a[1]+"\n")
+
+    ofile.close()
+
+
+
+# %%
+import os
+
+files = os.listdir(inputFolderExcels)
+for file in files:
+    if "xlsx" in file:
+        excelFile = inputFolderExcels+"/"+file
+        wellResultFile(excelFile,inputFolder)
+
+# %%
+annotationFiles = os.listdir(inputFolder)
+annotationFileName = ""
+for aFile in annotationFiles:
+    if "AllDetails" in aFile:
+        annotationFileName = aFile
+
+outAnnotationFileName = annotationFileName[0:annotationFileName.rindex("_")]+"_AJ.xlsx"
+
+
+annotationFile = pd.read_excel(inputFolder+"/"+annotationFileName)
 
 
 # %%
@@ -34,7 +84,7 @@ for i in annotationFile.index:
     
     annotationFile.at[i,"Annotator"] = "Ashwani"
         
-writer = pd.ExcelWriter(inputFolder+"/Combined_MLAnnotationData_AllDetails_20230414_AJ.xlsx")
+writer = pd.ExcelWriter(inputFolder+"/"+outAnnotationFileName)
 annotationFile.to_excel(writer,sheet_name="data",index=False)
 writer.close()
 
